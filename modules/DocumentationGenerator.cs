ee749831e50d4e60dadac6ee14efb59e8e16e539
Bluscream <Bluscream@users.noreply.github.com>
2023-10-04 01:40:26 +0200

    aaaaaaa

--- modules/DocumentationGenerator.cs
+++ modules/DocumentationGenerator.cs
@@ -4,16 +4,21 @@ using System.IO;
 using System.Reflection.Metadata;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using Mono.Cecil;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.MSBuild;
 
 using BBRAPIModules;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Net;
+using Mono.Cecil;
+using System.Reflection;
 
 namespace Bluscream {
+    [Module("Description", "2.0.2")]
     public class DocumentationGenerator : BattleBitModule {
-
 
         public abstract class BaseDefinition {
             public string? Type { get; set; }
@@ -22,7 +27,6 @@ namespace Bluscream {
             public bool? Abstract { get; set; }
             public bool? Virtual { get; set; }
         }
-
         public class ConfigStructure {
             public string? Name { get; set; }
             public FileInfo? Path { get; set; }
@@ -31,7 +35,6 @@ namespace Bluscream {
             public Dictionary<string, string> Properties { get; set; } = new();
             public Dictionary<string, object> Content { get; set; } = new();
         }
-
         public class CommandInfo {
             public string? Name { get; set; }
             public string? MethodName { get; set; }
@@ -42,15 +45,32 @@ namespace Bluscream {
             public FileInfo? Path { get; set; }
             public List<ModuleMetaData> Modules { get; set; } = new();
         }
-
         public class ModuleMetaData {
             public string? Name { get; set; }
+            public string? Description { get; set; }
             public string? Version { get; set; }
             public List<CommandInfo> Commands { get; set; } = new();
             public List<ConfigStructure> ConfigStructures { get; set; } = new();
         }
 
         public class ModuleParser {
+
+            public IReadOnlyList<dynamic> GetRuntimeModules() {
+                var list = new List<BattleBitModule>();
+                var moduleType = Assembly.GetEntryAssembly()?.GetType("BattleBitAPIRunner.Module");
+                var moduleListField = moduleType?.GetField("Modules", BindingFlags.Static | BindingFlags.Public);
+                if (moduleListField is null) return list;
+
+                return (IReadOnlyList<dynamic>)moduleListField.GetValue(null);
+            }
+
+            public List<ModuleFileMetaData> ParseModules() {
+                var moduleInfos = new List<ModuleFileMetaData>();
+                foreach (var module in GetRuntimeModules()) {
+                    moduleInfos.Add(new() { Path = module.ModuleFilePath, Modules = ParseModule(module.ModuleAssembly as Assembly) });
+                }
+                return moduleInfos;
+            }
             [Obsolete]
             public List<ModuleFileMetaData> ParseModules(DirectoryInfo directory) {
                 var moduleInfos = new List<ModuleFileMetaData>();
@@ -60,10 +80,44 @@ namespace Bluscream {
                 return moduleInfos;
             }
 
-            [Obsolete]
-            public List<ModuleMetaData> ParseModule(FileInfo file) {
+            public List<ModuleMetaData> ParseModule(FileInfo moduleFile) {
                 var moduleInfos = new List<ModuleMetaData>();
-                var assembly = Mono.Cecil.AssemblyDefinition.ReadAssembly(file.FullName);
+
+                // Use Roslyn to parse the C# file
+                var code = File.ReadAllText(moduleFile.FullName);
+                var tree = CSharpSyntaxTree.ParseText(code);
+                var root = (CompilationUnitSyntax)tree.GetRoot();
+
+                // Get all the classes in the file
+                var classes = root.DescendantNodes().OfType<ClassDeclarationSyntax>();
+
+                foreach (var classDeclaration in classes) {
+
+                    var moduleAttribute = classDeclaration.AttributeLists
+                        .SelectMany(a => a.Attributes)
+                        .FirstOrDefault(a => a.Name.ToString() == "ModuleAttribute");
+
+                        var attributeArguments = moduleAttribute?.ArgumentList?.Arguments;
+
+                        var moduleInfo = new ModuleMetaData {
+                        Name = classDeclaration.Identifier.Text,
+                        Description = attributeArguments.GetValueOrDefault()[0].ToString(),
+                        Version = attributeArguments.GetValueOrDefault()[1].ToString(),
+                        Commands = GetCommands(classDeclaration),
+                        ConfigStructures = GetConfigStructures(classDeclaration)
+                    };
+
+                    moduleInfos.Add(moduleInfo);
+                }
+
+                return moduleInfos;
+            }
+            [Obsolete]
+            public List<ModuleMetaData> ParseModule(Type moduleType) => ParseModule(moduleType.GetType().Assembly);
+            [Obsolete]
+            public List<ModuleMetaData> ParseModule(Assembly moduleAssembly) {
+                var assembly = Mono.Cecil.AssemblyDefinition.ReadAssembly(moduleAssembly.GetType().Assembly.Location);
+                var moduleInfos = new List<ModuleMetaData>();
                 foreach (var module in assembly.Modules) {
                     var moduleInfo = new ModuleMetaData {
                         Name = module.Name,
@@ -74,6 +128,36 @@ namespace Bluscream {
                     moduleInfos.Add(moduleInfo);
                 }
                 return moduleInfos;
+            }
+
+            public List<CommandInfo> GetCommands(ClassDeclarationSyntax classDeclaration) {
+                var commandInfos = new List<CommandInfo>();
+
+                // Get all the methods in the class
+                var methods = classDeclaration.DescendantNodes().OfType<MethodDeclarationSyntax>();
+
+                foreach (var method in methods) {
+                    // Get the CommandCallbackAttribute attribute if it exists
+                    var commandAttribute = method.AttributeLists
+                        .SelectMany(a => a.Attributes)
+                        .FirstOrDefault(a => a.Name.ToString() == "CommandCallbackAttribute");
+
+                    if (commandAttribute != null) {
+                        // Get the attribute arguments
+                        var attributeArguments = commandAttribute.ArgumentList.Arguments;
+
+                        var commandInfo = new CommandInfo() {
+                            MethodName = method.Identifier.Text,
+                            Name = attributeArguments[0].ToString(),
+                            Description = attributeArguments[1].ToString(),
+                            Permissions = (string[])attributeArguments[2].to,
+                        };
+
+                        commandInfos.Add(commandInfo);
+                    }
+                }
+
+                return commandInfos;
             }
             public List<CommandInfo> GetCommands(Mono.Cecil.ModuleDefinition module) {
                 var commandInfos = new List<CommandInfo>();
@@ -97,6 +181,42 @@ namespace Bluscream {
                 return commandInfos;
             }
 
+            public List<ConfigStructure> GetConfigStructures(ClassDeclarationSyntax classDeclaration) {
+                var configStructures = new List<ConfigStructure>();
+
+                // Get all the fields and properties in the class
+                var members = classDeclaration.Members;
+
+                foreach (var member in members) {
+                    if (member is FieldDeclarationSyntax fieldDeclaration) {
+                        // Get the field type and name
+                        var fieldType = fieldDeclaration.Declaration.Type.ToString();
+                        var fieldName = fieldDeclaration.Declaration.Variables.First().Identifier.ToString();
+
+                        // Create a new ConfigStructure object and add it to the list
+                        var configStructure = new ConfigStructure() {
+                            Name = fieldName,
+                            Fields = { { fieldType, fieldName } }
+                        };
+
+                        configStructures.Add(configStructure);
+                    } else if (member is PropertyDeclarationSyntax propertyDeclaration) {
+                        // Get the property type and name
+                        var propertyType = propertyDeclaration.Type.ToString();
+                        var propertyName = propertyDeclaration.Identifier.ToString();
+
+                        // Create a new ConfigStructure object and add it to the list
+                        var configStructure = new ConfigStructure() {
+                            Name = propertyName,
+                            Properties = { { propertyType, propertyName } }
+                        };
+
+                        configStructures.Add(configStructure);
+                    }
+                }
+
+                return configStructures;
+            }
             [Obsolete]
             public List<ConfigStructure> GetConfigStructures(Mono.Cecil.ModuleDefinition module) {
                 var configStructures = new List<ConfigStructure>();
