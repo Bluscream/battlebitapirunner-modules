b16a5de1fe55f9eb0d795f91d5dd496095954107
Bluscream <Bluscream@users.noreply.github.com>
2023-10-09 00:08:43 +0200

    add Kodiak to maprotation

--- modules/MapRotation.cs
+++ modules/MapRotation.cs
@@ -519,6 +519,13 @@ public class MapRotation : BattleBitModule
             ("FRONTLINE", new [] {MapSize._32vs32,MapSize._64vs64,MapSize._127vs127,}),
             ("CTF", new [] {MapSize._32vs32,MapSize._64vs64,MapSize._127vs127,}),
         }),
+        new MapInfo("Kodiak", new[]{
+            ("RUSH", new [] {MapSize._8v8,MapSize._16vs16,}),
+            ("CONQ", new [] {MapSize._16vs16,MapSize._32vs32,MapSize._127vs127,}),
+            ("INFCONQ", new [] {MapSize._16vs16,MapSize._32vs32,MapSize._127vs127,}),
+            ("DOMI", new [] { MapSize._16vs16, MapSize._32vs32,MapSize._127vs127,}),
+            ("CTF", new [] {MapSize._16vs16, MapSize._32vs32,MapSize._127vs127,}),
+        }),
     };
     }
 }
